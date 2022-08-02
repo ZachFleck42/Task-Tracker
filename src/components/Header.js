ba9556bc5d2a10445ba4d8b2304022f9eeef808a
Zachary Fleckenstein <zachfleck42@gmail.com>
2022-08-02 14:15:58 -0400

    Adds Button and Tasks components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,23 @@
-const Header = () => {
+import Button from './Button'
+
+const Header = (props) => {
+    const onClick = (e) => {
+        console.log('Click')
+    }
+
     return (
-        <header>
-            <h1>Task Tracker</h1>
+        <header className='header'>
+            <h1>Hello {props.name}</h1>
+            <Button color='green' text='Add' onClick={onClick}/>
         </header>
     )
 }
+
+
+// CSS in JS
+// const headingStyle = {
+//     color: 'red',
+//     backgroundColor: 'black'
+// }
 
 export default Header
